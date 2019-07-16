bf3aa6900f14b9314f7323f785261d2ef3a1b2eb
Dilip Mariya <dilip@acquaintsoft.com>
2019-07-16 10:43:24 +0530

    Initial-commit

---
+++ WatermarkMedia/WatermarkMedia.h
@@ -0,0 +1,19 @@
+//
+//  WatermarkMedia.h
+//  WatermarkMedia
+//
+//  Created by AGC on 16/07/19.
+//  Copyright © 2019 AGC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WatermarkMedia.
+FOUNDATION_EXPORT double WatermarkMediaVersionNumber;
+
+//! Project version string for WatermarkMedia.
+FOUNDATION_EXPORT const unsigned char WatermarkMediaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WatermarkMedia/PublicHeader.h>
+
+
